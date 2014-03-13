6d86dd724f79710ec4c1b1c4bb98074c6bef7e25
Stefan Bucur <stefan.bucur@epfl.ch>
2014-03-13 17:39:42 +0100

    Injecting a symbolic string in pipe.

--- hetenv/sender.lua
+++ hetenv/sender.lua
@@ -3,11 +3,13 @@
 require "symtc"
 
 function pipe_test (provider)
-   pipe_fd = io.open("pipey", "w")
+   local pipe_fd = io.open("pipey", "w")
    if pipe_fd == nil then
       io.stderr:write("Could not open pipe\n")
       error("Could not open pipe")
    end
+   local data = provider:getstring("message contents", "symval")
+   pipe_fd:write(data)
    pipe_fd:close()
 end
 
